327d893cd87f2898cabbdaf6c495f6eae8a67eb8
bagarwhyu <102155214+bagarwhyu@users.noreply.github.com>
2022-05-12 13:18:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// ===Luas-Keliling Persegi===
+
+var panjangPersegi = 10;
+var lebarPersegi = 5;
+
+var luasPersegi = panjangPersegi * lebarPersegi;
+var kelilingPersegi = 2 * (panjangPersegi + lebarPersegi);
+
+console.log(luasPersegi);
+console.log(kelilingPersegi);
+
+// ===Luas-Keliling Segitiga===
+
+var alasSegitiga = 5;
+var tinggiSegitiga = 8;
+
+var sisiSegitgaA = 10;
+var sisiSegitgaB = 10;
+var sisiSegitgaC = 10;
+
+var luasSegitiga = alasSegitiga * tinggiSegitiga / 2;
+var kelilingSegitiga = sisiSegitgaA + sisiSegitgaB + sisiSegitgaC;
+
+console.log(luasSegitiga);
+console.log(kelilingSegitiga);
+
+// ===Luas-Keliling Lingkaran===
+
+var jariJari = 14;
+
+var luasLingkaran = 22 / 7 * jariJari * jariJari;
+var kelilingLingkaran = 22 / 7 * 2 * jariJari;
+
+console.log(luasLingkaran);
+console.log(kelilingLingkaran);
+
+// ===Konversi Suhu===
+
+var celcius = 10;
+
+var celciusKelvin = celcius + 273;
