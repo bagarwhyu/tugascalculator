47000fca96f2d0e968f295ab11286cdda3dc7ed4
bagarwhyu <102155214+bagarwhyu@users.noreply.github.com>
2022-05-12 20:46:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,72 @@
 // ===Luas-Keliling Persegi===
 
-var panjangPersegi = 10;
-var lebarPersegi = 5;
+function luasPersegi() {
+    var panjangPersegi = parseInt (document.getElementById('panjangPersegi').value);
+    var lebarPersegi = parseInt (document.getElementById('lebarPersegi').value);
+    
+    var luasPersegi = panjangPersegi * lebarPersegi;
 
-var luasPersegi = panjangPersegi * lebarPersegi;
-var kelilingPersegi = 2 * (panjangPersegi + lebarPersegi);
+    console.log(luasPersegi);
+    alert('Luas Persegi = ' + luasPersegi);
+}
 
-console.log(luasPersegi);
-console.log(kelilingPersegi);
+function kelilingPersegi() {
+    var panjangPersegi = parseInt (document.getElementById('panjangPersegi').value);
+    var lebarPersegi = parseInt (document.getElementById('lebarPersegi').value);
+
+    var kelilingPersegi = 2 * (panjangPersegi + lebarPersegi);
+
+    console.log(kelilingPersegi);
+    alert('Keliling Persegi = ' + kelilingPersegi);
+}
 
 // ===Luas-Keliling Segitiga===
 
-var alasSegitiga = 5;
-var tinggiSegitiga = 8;
+function luasSegitiga() {
+    var alasSegitiga = parseInt (document.getElementById('alasSegitiga').value);
+    var tinggiSegitiga = parseInt (document.getElementById('tinggiSegitiga').value);
+    
+    var luasSegitiga = alasSegitiga * tinggiSegitiga / 2;
+    
+    console.log(luasSegitiga);
+    alert('Luas Segitiga = ' + luasSegitiga);
+}
 
-var sisiSegitgaA = 10;
-var sisiSegitgaB = 10;
-var sisiSegitgaC = 10;
-
-var luasSegitiga = alasSegitiga * tinggiSegitiga / 2;
-var kelilingSegitiga = sisiSegitgaA + sisiSegitgaB + sisiSegitgaC;
-
-console.log(luasSegitiga);
-console.log(kelilingSegitiga);
+function kelilingSegitiga() {
+    var sisiSegitigaA = parseInt (document.getElementById('sisiSegitigaA').value);
+    var sisiSegitigaB = parseInt (document.getElementById('sisiSegitigaB').value);
+    var sisiSegitigaC = parseInt (document.getElementById('sisiSegitigaC').value);
+    
+    var kelilingSegitiga = sisiSegitigaA + sisiSegitigaB + sisiSegitigaC;
+    
+    console.log(kelilingSegitiga);
+    alert('Keliling Segitiga = ' + kelilingSegitiga);
+}
 
 // ===Luas-Keliling Lingkaran===
 
-var jariJari = 14;
+function luasLingkaran() {
+    var jariJari = parseInt (document.getElementById('jariJari').value);
 
-var luasLingkaran = 22 / 7 * jariJari * jariJari;
-var kelilingLingkaran = 22 / 7 * 2 * jariJari;
+    var luasLingkaran = 22 / 7 * jariJari * jariJari;
 
-console.log(luasLingkaran);
-console.log(kelilingLingkaran);
+    console.log(luasLingkaran);
+    alert('Luas Lingkaran = ' + luasLingkaran);
+}
+
+function kelilingLingkaran() {
+    var jariJari = parseInt (document.getElementById('jariJari').value);
+    
+    var kelilingLingkaran = 22 / 7 * 2 * jariJari;
+
+    console.log(kelilingLingkaran);
+    alert('Keliling Lingkaran = ' + kelilingLingkaran);
+}
 
 // ===Konversi Suhu===
 
 var celcius = 10;
 
 var celciusKelvin = celcius + 273;
+
+console.log(celciusKelvin);
